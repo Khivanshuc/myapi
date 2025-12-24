ad64cc165bfccf6720c4fecdf75503c267342308
Khivanshuc <khivanshuc@binomialsolutions.com>
2025-12-24 15:19:13 +0530

    Initial Project commit

---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+
+    //Controller Routing
+    [Route("api/[controller]")]  
+
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "MonsterASP API is working!";
+        }
+
+
+        //Method Routing
+        [HttpGet("check")]
+        public string Check()
+        {
+            return "Checking API is working!";
+        }
+    }
+}
